6ea216fa3d7367cfc6d3c9017ad7bfd201309a7a
NateAgcaoili <75859423+NateAgcaoili@users.noreply.github.com>
2021-04-01 21:54:15 -0400

    First semi-working version of game completed

--- SimonSays/app/src/main/java/com/georgiasouthern/simonsays/MainActivity.java
+++ SimonSays/app/src/main/java/com/georgiasouthern/simonsays/MainActivity.java
@@ -17,9 +17,13 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     int currentRound;
+    int playerIndex;
+    int blueColor;
+    int redColor;
     boolean running;
     boolean playerTurn;
     Button[] gameButtons;
+    Drawable[] backgrounds;
     ArrayList<Integer> simonSequence = new ArrayList<Integer>();
 
     @Override
@@ -29,12 +33,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         currentRound = 1;
+        playerIndex = 0;
+        blueColor = getResources().getColor(R.color.theme_blue);
+        redColor = getResources().getColor(R.color.theme_red);
         running = true;
         playerTurn = false;
         gameButtons = new Button[]{
                 findViewById(R.id.butOne), findViewById(R.id.butTwo), findViewById(R.id.butThree),
                 findViewById(R.id.butFour), findViewById(R.id.butFive), findViewById(R.id.butSix),
                 findViewById(R.id.butSeven), findViewById(R.id.butEight), findViewById(R.id.butNine)};
+        backgrounds = new Drawable[]{
+                getResources().getDrawable(R.drawable.title_bg),  getResources().getDrawable(R.drawable.game_bg),
+                getResources().getDrawable(R.drawable.game_over_bg)};
     }
 
     public void playClick(View view) {
@@ -49,23 +59,64 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void toggleButtons() {
+        if (playerTurn){
+            for (int i = 0; i < 9; i++) {
+                gameButtons[i].setEnabled(true);
+            }
+        } else {
+            for (int i = 0; i < 9; i++) {
+                gameButtons[i].setEnabled(false);
+            }
+        }
+
+    }
+
     public void openGameScreen() {
         showButtons();
         findViewById(R.id.playButton).setVisibility((View.GONE));
         findViewById(R.id.scoresButton).setVisibility((View.GONE));
         findViewById(R.id.aboutButton).setVisibility((View.GONE));
-        setActivityBackground(getResources().getDrawable(R.drawable.game_bg));
+        setActivityBackground(backgrounds[1]);
         running = true;
+        playGame();
     }
 
     public void playGame() {
         if (!running) {
+            gameOver();
+        } else if (!playerTurn) {
+            addToSequence();
+            new DisplaySequence().execute();
+        } else if (playerTurn) {
+            if (playerIndex == currentRound) {
+                currentRound++;
+                playerIndex = 0;
+                playerTurn = false;
+                toggleButtons();
+                playGame();
+            }
+        }
+    }
 
+    public void gameButtonClick(View view) {
+        Button buttonClicked = findViewById(view.getId());
+        if (gameButtons[simonSequence.get(playerIndex)] == buttonClicked) {
+            playerIndex++;
+            playGame();
+        } else {
+            running = false;
+            playGame();
         }
     }
 
     public void gameOver() {
-        
+        playerTurn = false;
+        for (int i = 0; i < 9; i++) {
+            gameButtons[i].setBackgroundColor(redColor);
+        }
+        toggleButtons();
+        setActivityBackground(backgrounds[2]);
     }
 
     public void setActivityBackground(Drawable background) {
@@ -78,20 +129,15 @@ public class MainActivity extends AppCompatActivity {
         simonSequence.add(newInt);
     }
 
-    public void displaySequence() throws InterruptedException {
-
-    }
-
-    public class GameAsyncTask extends AsyncTask<Integer, Integer, Double> {
+    public class DisplaySequence extends AsyncTask<Integer, Integer, Double> {
 
         @Override
         protected Double doInBackground(Integer... integers) {
             for (int i = 0; i < simonSequence.size(); i++) {
-                publishProgress(i, Color.GREEN);
-                SystemClock.sleep(5000);
-                publishProgress(i, Color.BLUE);
+                publishProgress(i, Color.WHITE);
+                SystemClock.sleep(1000);
+                publishProgress(i, blueColor);
             }
-
             return null;
         }
 
@@ -100,7 +146,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         protected void onPostExecute(Double result) {
-
+            playerTurn = true;
+            toggleButtons();
+            playGame();
         }
     }
 }

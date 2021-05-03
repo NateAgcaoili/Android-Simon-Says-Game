7a4a6a026b36f206eba1c4b3ecbe0932a4fc2a5b
NateAgcaoili <75859423+NateAgcaoili@users.noreply.github.com>
2021-05-03 02:07:13 -0400

    Added link to project GitHub repository in about page

--- SimonSays/app/src/main/java/com/georgiasouthern/simonsays/AboutActivity.java
+++ SimonSays/app/src/main/java/com/georgiasouthern/simonsays/AboutActivity.java
@@ -3,6 +3,7 @@ package com.georgiasouthern.simonsays;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -17,6 +18,12 @@ public class AboutActivity extends AppCompatActivity {
 
     public void menuClick(View view) {
         Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void repositoryClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://github.com/NateAgcaoili/Android-Simon-Says-Game"));
         startActivity(intent);
     }
 }
